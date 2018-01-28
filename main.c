04e47fa6169a759242a0ebdb1dad54a6b273b4d7
MaGaroo <amoomajid99@gmail.com>
2018-01-28 23:08:18 +0330

    initial main.c file

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+#include "SDL2_gfxPrimitives.h"
+
+#define WIDTH 720
+#define HEIGHT 480
+#define CELL_SIZE 10
+#define BODY_RADIUS 9
+#define FOOD_RADIUS 4
+#define EYE_RADIUS 1
+
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
